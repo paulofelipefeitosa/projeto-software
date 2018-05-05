0daaab3e96ee1dada567a165fa1391574f087d59
Paulo Felipe <paulofelipe.feitosa@gmail.com>
2018-05-05 16:57:23 -0300

    Adding login page

--- lab01/front-end/view.js
+++
@@ -1,8 +0,0 @@
-
-function toogleMessageCheckbox() {
-  checkboxes = document.getElementsByName('message_checkbox');
-  source = document.getElementById('message_checkboxes')
-  for(var i = 0, n = checkboxes.length; i < n; i++) {
-    checkboxes[i].checked = source.checked;
-  }
-}
